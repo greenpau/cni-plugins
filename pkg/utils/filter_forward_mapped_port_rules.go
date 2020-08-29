9b8d49d708b2b80dc40b848930e5d7251da2141f
Paul Greenberg <greenpau@outlook.com>
2020-08-29 14:44:19 -0400

    add port match

--- pkg/utils/filter_forward_mapped_port_rules.go
+++ pkg/utils/filter_forward_mapped_port_rules.go
@@ -5,6 +5,9 @@ import (
 	//"github.com/davecgh/go-spew/spew"
 	"github.com/google/nftables"
 	"github.com/google/nftables/expr"
+
+	"github.com/google/nftables/binaryutil"
+	"golang.org/x/sys/unix"
 	"net"
 )
 
@@ -77,6 +80,43 @@ func AddFilterForwardMappedPortRules(v, tableName, chainName string, addr net.IP
 	} else {
 		return fmt.Errorf("IPv6 is unsupported")
 	}
+
+	r.Exprs = append(r.Exprs, &expr.Meta{
+		Key:      expr.MetaKeyL4PROTO,
+		Register: 1,
+	})
+
+	switch pm.Protocol {
+	case "tcp":
+		r.Exprs = append(r.Exprs, &expr.Cmp{
+			Op:       expr.CmpOpEq,
+			Register: 1,
+			Data:     []byte{unix.IPPROTO_TCP},
+		})
+	case "udp":
+		r.Exprs = append(r.Exprs, &expr.Cmp{
+			Op:       expr.CmpOpEq,
+			Register: 1,
+			Data:     []byte{unix.IPPROTO_UDP},
+		})
+	default:
+		return fmt.Errorf("unsupported protocol: %s", pm.Protocol)
+	}
+
+	// [ payload load 2b @ transport header + 2 => reg 1 ]
+	r.Exprs = append(r.Exprs, &expr.Payload{
+		DestRegister: 1,
+		Base:         expr.PayloadBaseTransportHeader,
+		Offset:       2, // TODO
+		Len:          2, // TODO
+	})
+
+	// [ cmp eq reg 1 0x0000e60f ]
+	r.Exprs = append(r.Exprs, &expr.Cmp{
+		Op:       expr.CmpOpEq,
+		Register: 1,
+		Data:     binaryutil.BigEndian.PutUint16(uint16(pm.ContainerPort)),
+	})
 
 	r.Exprs = append(r.Exprs, &expr.Counter{})
 
